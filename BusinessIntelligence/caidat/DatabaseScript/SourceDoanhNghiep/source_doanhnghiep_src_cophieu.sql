a6dcf28e17e68fa90794d36a14f18994e13ce265
sihlp <sihlp@vng.com.vn>
2021-07-22 17:10:42 +0700

    Update document

--- BusinessIntelligence/caidat/DatabaseScript/SourceDoanhNghiep/source_doanhnghiep_src_cophieu.sql
+++ BusinessIntelligence/caidat/DatabaseScript/SourceDoanhNghiep/source_doanhnghiep_src_cophieu.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `src_cophieu`
 --
-
+USE source_doanhnghiep
 DROP TABLE IF EXISTS `src_cophieu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
